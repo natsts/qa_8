92b93524f1fdc47d61a5ed54f8da08ae8fd2cffe
Leusheva Natalya <leusheva_nv@taximaxim.ru>
2023-02-23 18:45:55 +0500

    add allure

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,45 +1,5 @@
 import pytest
 from selene.support.shared import browser
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-from selene import Browser, Config
-from utils import attach
-
-
-def pytest_addoption(parser):
-    parser.addoption(
-        '--browser_version',
-        default='100.0'
-    )
-
-
-@pytest.fixture(scope='function')
-def setup_browser(request):
-    browser_version = request.config.getoption('--browser_version')
-    options = Options()
-    selenoid_capabilities = {
-        "browserName": "chrome",
-        "browserVersion": browser_version,
-        "selenoid:options": {
-            "enableVNC": True,
-            "enableVideo": True
-        }
-    }
-    options.capabilities.update(selenoid_capabilities)
-
-    driver = webdriver.Remote(
-        command_executor="https://user1:1234@selenoid.autotests.cloud/wd/hub",
-        options=options
-    )
-    browser = Browser(Config(driver))
-
-    yield browser
-
-    attach.add_html(browser)
-    attach.add_screenshot(browser)
-    attach.add_logs(browser)
-    attach.add_video(browser)
-    browser.quit()
 
 
 @pytest.fixture()
