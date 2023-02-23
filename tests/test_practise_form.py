f62de1c35414b124d765f99777381eaa1c865b7b
Leusheva Natalya <leusheva_nv@taximaxim.ru>
2023-02-23 16:32:42 +0500

    add allure

--- tests/test_practise_form.py
+++ tests/test_practise_form.py
@@ -1,14 +1,32 @@
 import allure
 from allure_commons.types import Severity
 from selene.support.shared import browser
-
+from selenium.webdriver.chrome.options import Options
+from selenium import webdriver
 from model.page import practice_form
+from utils import attach
 
 
 @allure.tag("web")
 @allure.severity(Severity.NORMAL)
 @allure.label("owner", "natsts")
 def test_practise_form(size_browser):
+    options = Options()
+    selenoid_capabilities = {
+        "browserName": "chrome",
+        "browserVersion": "100.0",
+        "selenoid:options": {
+            "enableVNC": True,
+            "enableVideo": False
+        }
+    }
+    options.capabilities.update(selenoid_capabilities)
+    driver = webdriver.Remote(
+        command_executor="https://user1:1234@selenoid.autotests.cloud/wd/hub",
+        options=options)
+
+    browser.config.driver = driver
+
     with allure.step('Ореn practice form'):
         practice_form.open_form()
 
@@ -40,3 +58,7 @@ def test_practise_form(size_browser):
             'Moscow',
             'NCR Delhi'
         )
+
+    attach.add_html(browser)
+    attach.add_screenshot(browser)
+    attach.add_logs(browser)
